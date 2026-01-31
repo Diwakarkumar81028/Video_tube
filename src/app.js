f1de395e3179c85ebf9f4c043e4f6a53b283d2fe
Diwakar Kumar <diwakarkumars1234@gmail.com>
2026-01-31 22:47:32 +0530

    testing is done

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
-//routes;
+//user routes;
 import userRouter from "./routes/user.routes.js"
 app.use("/api/v1/users",userRouter)//--> user.routes.js me jayega
 export default app;
