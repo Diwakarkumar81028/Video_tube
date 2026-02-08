d49639269fa1475b2211b9844a55f398c9a0df38
Diwakar Kumar <diwakarkumars1234@gmail.com>
2026-02-09 00:02:53 +0530

    add refresh token end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {User} from "../models/user.model.js";
 import {cloudinary_upload} from "../utils/cloudinary.js";
 import { apiresponse } from "../utils/apiresponse.js";
 import validator from "validator";
+import jwt from "jsonwebtoken";
 
 async function generateAccessAndRefreshToken(userId) {
     try{
@@ -92,56 +93,8 @@ async function loginUser(req,res) {
     //4.if user exits or not
     //5.validate password
     //6. generate access token and refresh token
-    //6. loged in
+    //6. logged in
     //7. send in cookies
-    
-    //1.
-    // const {username,email,password}=req.body;
-    // if(!username && !email){
-    //     throw new apierror(400,"username or email is required");
-    // }
-    // if(!password){
-    //     throw new apierror(400,"password is required")
-    // }
-    // //2.
-    // const user=User.findOne({
-    //     $or:[{email},{username}]
-    // })
-    // if(!user) {
-    //     throw new apierror(400,"user is not found")
-    // }
-    // //3.
-    // const isPassordValid=await user.isPasswordCorrect(password)
-    // if(!isPassordValid){
-    //     throw new apierror(400,"password is incorrect")
-    // }
-    // //4.
-    // const refreshToken=await user.generateRefreshToken();
-    // const accessToken=await user.generateAccessToken();
-    // //5.
-    // if(!refreshToken || !accessToken){
-    //     throw new apierror(500,"error is found while generating the accesToken or refreshToken")
-    // }
-    // await user.save({validateBeforeSave:false});
-    // user.refreshToken=refreshToken
-    // //6.
-    // //7.
-    // const options={
-    //     httpOnly:true,
-    //     secure:true
-    // } 
-    // //
-    // return res.status(200)
-    // .cookie("accessToken",accessToken,options)
-    // .cookie("refreshToken",refreshToken,options)
-    // .json(
-    //     new apiresponse(
-    //         200,{
-    //             user
-    //         },
-    //         "User logged In Successfully"
-    //     )
-    // )
 
     //1.
      const {username,email,password}=req.body;
@@ -193,38 +146,76 @@ async function loginUser(req,res) {
 export {loginUser}
 
 //3. logout
-// async function logoutUser(req, res) {
-//   const userid = req.user._id;
-//   console.log(req);
-//   await User.findByIdAndUpdate(
-//     userid,
-//     {
-//       $set: {
-//         refreshToken: undefined,
-//       },
-//     },
-//     {
-//       new: true,
-//     }
-//   );
-//   //
-//   //cookies
-//    //by default any one can modify the cookies on frontend;
-//    const options={
-//     httpOnly:true,// modifiable only by server not by frontend
-//     secure:true//-->
-//    }
+async function logoutUser(req, res) {
+  const userid = req.user._id;
+  console.log(req);
+  await User.findByIdAndUpdate(
+    userid,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  //
+  //cookies
+   //by default any one can modify the cookies on frontend;
+   const options={
+    httpOnly:true,// modifiable only by server not by frontend
+    secure:true//-->
+   }
 
-//    return res.status(200)
-//    .clearCookie("accessToken",options)
-//    .clearCookie("refreshToken",options)
-//    .json(new apiresponse(200,{},"User logged Out"))
-// }
-
-async function logoutUser(req,res) {
-    console.log("logout",req);
-    res.status(200).json(
-        200,{},"user logout successsfully"
-    )
+   return res.status(200)
+   .clearCookie("accessToken",options)
+   .clearCookie("refreshToken",options)
+   .json(new apiresponse(200,{},"User logged Out"))
 }
 export {logoutUser}
+
+//4.
+const refreshAccessToken=async function(req,res) {
+try {
+        //1. get refrresh token
+        const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken;
+       if(!incomingRefreshToken){
+        throw new apierror(401,"unauthorized request")
+       }
+       //2. verify refresh token;
+      const decodedToken= jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET);
+       //3. user 
+       const user=await User.findById(decodedToken?._id);
+       if(!user) {
+        throw new apierror(400,"Invalid refreshToken")
+       }
+       //4.match refrsh token
+       if(incomingRefreshToken!==user?.refreshToken){
+        throw new apierror(401,"Refresh Token is expired")
+       }
+       //5.gen access token
+       const {accessToken,newrefreshToken}=await generateAccessAndRefreshToken(user._id);
+       //
+       const options={
+        httpOnly:true,
+        secure:true
+       }
+    
+       return res.status(200)
+       .cookie("accessToken",accessToken,options)
+       .cookie("refreshToken",newrefreshToken.options)
+       .json(
+          new apiresponse(
+            200,
+            {accessToken,refreshToken:newrefreshToken},
+            "Access token refreshed"
+          )
+       )
+} 
+catch (error) {
+    throw new apierror(401,error?.message||"invalid refreshtoken")
+}
+}
+
+export{refreshAccessToken}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {refreshAccessToken} from "../controllers/user.controller.js"
 const router=Router()
 
 //1.register route
@@ -22,6 +23,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser) 
 
 //3. loggout
-// router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/logout").post(logoutUser);
+router.route("/logout").post(verifyJWT,logoutUser)
+//4.
+router.route("/refresh-token").post(refreshAccessToken)
 export default router;
